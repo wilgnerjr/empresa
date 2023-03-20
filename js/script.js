e50b8dd42b3a790b60e799074e838c52a7ecb599
WilgnerJR <wilgnerbento@gmail.com>
2023-03-19 23:54:50 -0400

    update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // ELEMENTO 01
 const menu = document.querySelector('.menu');
 const navMenu = document.querySelector('.nav-menu');
+var navinicio = document.querySelectorAll('#nav-menu');
 
 menu.addEventListener('click', () => {
     menu.classList.toggle('ativo');
@@ -8,4 +9,7 @@ menu.addEventListener('click', () => {
 })
 
 //ELEMENTO 02
-
+function esconder(){
+    menu.classList.toggle('ativo');
+    navMenu.classList.toggle('ativo');
+}
